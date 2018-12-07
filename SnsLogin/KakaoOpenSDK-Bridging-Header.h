61760530d73a85a1d20c8ff2b98c593e2d7f4758
Unknown <min.songkyung@gmail.com>
2018-12-07 18:05:27 +0900

    카카오 설정

---
+++ SnsLogin/KakaoOpenSDK-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  KakaoOpenSDK-Bridging-Header.h
+//  SnsLogin
+//
+//  Created by GlobalHumanism on 2018. 12. 7..
+//  Copyright © 2018년 GlobalHumanism. All rights reserved.
+//
+
+#ifndef KakaoOpenSDK_Bridging_Header_h
+#define KakaoOpenSDK_Bridging_Header_h
+
+#import <KakaoOpenSDK/KakaoOpenSDK.h>
+
+#endif /* KakaoOpenSDK_Bridging_Header_h */

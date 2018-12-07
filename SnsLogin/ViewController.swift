27989e050daf284637b2c9623f21c8bbde60de5e
songkyung min <min.songkyung@gmail.com>
2018-12-07 15:55:30 +0900

    first commit

---
+++ SnsLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnsLogin
+//
+//  Created by GlobalHumanism on 2018. 12. 7..
+//  Copyright © 2018년 GlobalHumanism. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
